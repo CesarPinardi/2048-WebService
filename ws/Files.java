b25bb84552c9f1159b39e097dcdb02129217913d
César Augusto Pinardi <cesar.pinardi@gmail.com>
2019-12-04 14:05:37 -0300

    Add files via upload

---
+++ ws/Files.java
@@ -0,0 +1,69 @@
+package ws;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Formatter;
+import java.util.Scanner;
+
+
+//Cria arquivos para salvar a direcao desejada
+public class Files {
+
+    public Files() {
+
+    }
+
+    //Caminho que será salvo o arquivo
+    static String path = "/home/cesar/NetBeansProjects/WebService2048/src/javadirection.txt";
+    //Cria arquivo
+    public static void createDir() {
+        try {
+            //Cria arquivo com sua localização e nome
+            File file = new File(path);
+
+            //Para poder escrever no arquivo
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            bw.write("" + 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+    //Pega a string do arquivo
+    public static String loadDir() {
+        String dir;
+        try {
+            File file = new File(path);
+            Scanner sc = new Scanner(file);
+            
+            if (!file.isFile()) {
+                createDir();
+            }
+            dir = sc.next();
+            return dir;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    
+    //Modifica a direção
+    public static boolean setDir(String Texto) {
+        FileWriter output = null;
+        try {
+            Formatter file = new Formatter(path);
+             
+            file.format("" + Texto);
+            file.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
